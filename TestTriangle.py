f73674ea8e24720de2b9709687a56f788bfab2ac
wzhang62 <70457811+wzhang62@users.noreply.github.com>
2020-10-06 20:43:46 -0400

    Update TestTriangle.py

--- TestTriangle.py
+++ TestTriangle.py
@@ -1,21 +1,12 @@
 # -*- coding: utf-8 -*-
-"""
-Updated Jan 21, 2018
-The primary goal of this file is to demonstrate a simple unittest implementation
 
-@author: jrr
-@author: rk
-"""
 
 import unittest
 
 from Triangle import classifyTriangle
 
-# This code implements the unit test functionality
-# https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
 class TestTriangles(unittest.TestCase):
-    # define multiple sets of tests as functions with names that begin
     def testInvalidInput(self):
         self.assertEqual(classifyTriangle(2,3,0),'invalid input')
 
