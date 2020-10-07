0470b3c3d03293c6757be69ef1b378fc8dab97ee
wzhang62 <70457811+wzhang62@users.noreply.github.com>
2020-10-06 20:42:37 -0400

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jan 14 13:44:00 2016
-Updated Jan 21, 2018
 
-The primary goal of this file is to demonstrate a simple python program to classify triangles
-
-@author: jrr
-@author: rk
-"""
 
 def validinput(a):
     if a > 0:
@@ -25,22 +17,7 @@ def validlength(a,b,c):
 
 def classifyTriangle(a,b,c):
 
-    """
-    Your correct code goes here...  Fix the faulty logic below until the code passes all of
-    you test cases.
-
-    This function returns a string with the type of triangle from three integer values
-    corresponding to the lengths of the three sides of the Triangle.
-
-    return:
-        If all three sides are equal, return 'Equilateral'
-        If exactly one pair of sides are equal, return 'Isoceles'
-        If no pair of  sides are equal, return 'Scalene'
-        If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
-
-      BEWARE: there may be a bug or two in this code
-    """
+   
     if validinput(a) and validinput(b) and validinput(c) and validlength(a,b,c) is True:
         if a == b and b == c:
             return 'equilateral triangle'
